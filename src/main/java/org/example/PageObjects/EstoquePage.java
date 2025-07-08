e6afa81986e087ebdba763f46d3cbe2efeef0d44
AndersonFreireFlores <andersonfflores75@gmail.com>
2025-07-07 22:52:02 -0300

    teste estoque

--- src/main/java/org/example/PageObjects/EstoquePage.java
+++ src/main/java/org/example/PageObjects/EstoquePage.java
@@ -7,7 +7,63 @@ public class EstoquePage {
     public String pathBotaoCriarEstoque = "/html/body/app-root/app-container/main/div/app-movimentacao/form/div/div[5]/div/button[3]";
     public String pathBotaoFiltrar = "/html/body/app-root/app-container/main/div/app-movimentacao/form/div/div[5]/div/button[2]";
     public String pathBotaoSalvar = "/html/body/app-root/app-container/main/div/app-movimentacao/div[1]/div/div/form/div[2]/button[2]";
+    public String pathBotaoEstoque = "/html/body/app-root/app-header/header/div/div/ul[1]/li[7]/a/i";
+    public String pathBotaoExcluir = "/html/body/app-root/app-container/main/div/app-movimentacao/div[2]/table/tbody/tr/td[8]/i";
+    public String pathBotaoConfirmarExcluir = "/html/body/app-root/app-container/main/div/app-movimentacao/app-confirm-modal/div/div/div/div[3]/button[2]";
+
 
     public Dsl dsl = new Dsl();
+
+    public void clicarEstoque(){
+        dsl.clicarBotao(pathBotaoEstoque);
+    }
+
+    public void clicarCriarEstoque() {
+        dsl.clicarBotao(pathBotaoCriarEstoque);
+    }
+
+    public void clicarExcluir(){
+        dsl.clicarBotao(pathBotaoExcluir);
+    }
+
+    public void clicarBotaoConfirmarExcluir(){
+        dsl.clicarBotao(pathBotaoConfirmarExcluir);
+    }
+
+    public void clicarFiltrar(){
+        dsl.clicarBotao(pathBotaoFiltrar);
+    }
+
+    public void clicarSalvar(){
+        dsl.clicarBotao(pathBotaoSalvar);
+    }
+
+    public void setData(String data){
+        dsl.escrever("cadastroData",data);
+    }
+
+    public void setItem(String nome){
+        dsl.selecionarComScroll("cadastro_elemento",nome);
+    }
+
+    public void setTipo(String tipo){
+        dsl.selecionarComScroll("cadastroTipo", tipo);
+    }
+
+    public void setSubItem(String item){
+        dsl.selecionarComScroll("cadastro_elementoItem",item);
+    }
+
+    public void setQtde(double qtde){
+        dsl.escrever("cadastroQuantidade", String.valueOf(qtde));
+    }
+
+    public void setObs(String obs){
+        dsl.escrever("observacoes", obs);
+    }
+
+    public void setItemFiltro(String item){
+        dsl.selecionarComScroll("filtro_elemento", item);
+    }
 
 }
---
+++ src/main/java/org/example/Testes/TesteEstoque.java
@@ -0,0 +1,88 @@
+package org.example.Testes;
+
+import org.example.DSL.DriverFactory;
+import org.example.DSL.Dsl;
+import org.example.PageObjects.EstoquePage;
+import org.example.PageObjects.LoginPage;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TesteEstoque {
+
+    private Dsl dsl;
+    private static LoginPage loginPage = new LoginPage();
+    private static EstoquePage estoquePage = new EstoquePage();
+
+    private String email = "anderson.flores@aluno.feliz.ifrs.edu.br";
+    private String senha = "ABC123";
+
+    @Before
+    public void inicializar() {
+        DriverFactory.getDriver().get("http://35.209.123.161/front");
+        dsl = new Dsl();
+
+        loginPage.setEmail(email);
+        loginPage.setSenha(senha);
+        loginPage.logar();
+        estoquePage.clicarEstoque();
+    }
+
+    @After
+    public void finalizar(){
+        DriverFactory.killDriver();
+    }
+
+    @Parameterized.Parameter
+    public String data;
+    @Parameterized.Parameter(value = 1)
+    public double qtde;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getCollection(){
+        return Arrays.asList(new Object[][]{
+                {"2025-07-09", 1.5},
+                {"2025-08-10", 9.9}
+        });
+    }
+
+    @Test
+    public void test1_criarEstoque(){
+
+        estoquePage.clicarCriarEstoque();
+        estoquePage.setData(data);
+        estoquePage.setTipo("Entrada");
+        estoquePage.setItem("Item DG 4165a57b");
+        estoquePage.setSubItem("Subitem DG bf0ebbe7");
+        estoquePage.setQtde(qtde);
+        estoquePage.setObs("É importante observar que essa observação é extremamente observativa sobre sua observabilidade...");
+        estoquePage.clicarSalvar();
+
+        estoquePage.setItemFiltro("Item DG 4165a57b");
+        estoquePage.clicarFiltrar();
+
+        Assert.assertEquals("AndersonFlores", dsl.obterTexto(
+                By.xpath("/html/body/app-root/app-container/main/div/app-movimentacao/div[2]/table/tbody/tr/td[3]"
+                )));
+    }
+
+    @Test
+    public void test3_excluirEstoque(){
+        estoquePage.setItemFiltro("Item DG 4165a57b");
+        estoquePage.clicarExcluir();
+        estoquePage.clicarBotaoConfirmarExcluir();
+
+
+        Assert.assertEquals("Movimentação excluída com sucesso!\n", dsl.obterTexto(
+                By.xpath("/html/body/app-root/app-container/main/div/app-movimentacao/p-toast/div/p-toastitem/div/div/div/div[2]")));
+    }
+
+
+}
